8a85259a5afab38896e324eef44da22061b87c97
hcw3737 <77056802+hcw3737@users.noreply.github.com>
2021-06-03 14:40:45 +0900

    Create 5904.py

---
+++ 5904.py
@@ -0,0 +1,10 @@
+# from sys import stdin
+# input = stdin.readline
+
+N = int(input())
+moo = 'm o o m o o o m o o m o o o o m o o m o o o m o o m o o o o o '
+moo_list = list(map(str,moo.split(' ')))
+
+def
+    #시작
+    if k == 0 :
