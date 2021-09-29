e4f2f31ac56911e6163faa532a6f52a535f557ba
hcw3737 <77056802+hcw3737@users.noreply.github.com>
2021-09-30 00:43:44 +0900

    Update 카펫.py

--- programmers/lv_2/카펫.py
+++ programmers/lv_2/카펫.py
@@ -1,1 +1,37 @@
 # 카펫
+
+# 카펫. 완전탐색
+
+def solution(brown, yellow):
+    # 노란색 사이즈 종류
+    divide = []
+    divide_back = []
+    for i in range(1, int(yellow**(1/2))+1):
+        if (yellow % i == 0) :
+            divide.append(i)
+            if (i != (yellow//i)):
+                divide_back.append(yellow//i)
+
+    size = divide + divide_back[::-1]
+
+    # 노란색 사이즈 찾기
+    for i in range(len(size)) :
+        row = size[i]
+        col = size[-(i+1)]
+        if row > col:
+            break
+        # 브라운 사이즈와 일치
+        if row+col+2 == brown//2 :
+            return [col+2, row+2]
+
+
+# 최적화
+
+def solution(brown, yellow):
+    # 노란색 사이즈 종류
+    for i in range(1, int(yellow**(1/2))+1):
+        if (yellow % i == 0) :
+            row = i
+            col = yellow//i
+            if row+col+2 == brown//2 :
+                return [col+2, row+2]
