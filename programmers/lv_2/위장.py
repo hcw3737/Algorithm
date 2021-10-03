e753f4000778b2d7f09e872424e5c22444b693b4
hcw3737 <77056802+hcw3737@users.noreply.github.com>
2021-10-04 01:31:59 +0900

    Create 위장.py

---
+++ programmers/lv_2/위장.py
@@ -0,0 +1,15 @@
+# 위장 # 해시 # 42578
+
+def solution(clothes):
+    cloth = dict()
+    for info in clothes :
+        if info[1] in cloth.keys():
+            cloth[info[1]]+=1
+        else :
+            cloth[info[1]] = 1
+
+    case = 1
+    for i in cloth.values():
+        case *= (i+1)
+
+    return case - 1
