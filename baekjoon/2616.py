8744247fdc700f667fe7be446af34f63f5b98888
hcw3737 <77056802+hcw3737@users.noreply.github.com>
2022-03-22 23:42:01 +0900

    Create 2616.py

---
+++ baekjoon/2616.py
@@ -0,0 +1,19 @@
+# DP # 소형기관차
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+train = [0] + list(map(int, input().split()))
+m = int(input())
+
+dp = [[0]*(n+1) for _ in range(4)]
+
+for i in range(1,4):
+    for j in range(i*m, n+1):
+        if i == 1 :
+            dp[i][j] = sum(train[j-m+1:j+1])
+        else :
+            dp[i][j] = max(dp[i-1][j-m]+sum(train[j-m+1:j+1]),dp[i][j-1])
+
+print(dp[3][-1])
