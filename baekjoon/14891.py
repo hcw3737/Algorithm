8242bbd4b33acbdda961e19ac06a083ebcf78593
hcw3737 <77056802+hcw3737@users.noreply.github.com>
2022-04-14 23:57:07 +0900

    Create 14891.py

---
+++ baekjoon/14891.py
@@ -0,0 +1,28 @@
+
+# https://www.acmicpc.net/problem/14891
+# 삼성 # 톱니바퀴
+
+import copy
+
+arr = [list(input()) for _ in range(4)]
+k = int(input())
+info = []
+for _ in range(k) :
+    n, m = map(int, input().split())
+    info.append([n-1,m])
+
+arr_2 = copy.deepcopy(arr)
+
+def rotate():
+
+
+
+def main(arr, k, info) :
+
+    for i, r in info:
+        # 반시계
+        if r == -1:
+             
+        # 시계
+        else :
+
